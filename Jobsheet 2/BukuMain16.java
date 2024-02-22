626044dbafc391ab52a81b24e858bc077cacae7c
HaQi <luqmanananta745@gmail.com>
2024-02-22 12:56:33 +0700

    4

--- Jobsheet 2/BukuMain16.java
+++ Jobsheet 2/BukuMain16.java
@@ -19,9 +19,13 @@ public class BukuMain16{
     }
     else
     System.out.println("Invalid");
-    
+
     Buku16 bk2 = new Buku16("Self Reward", "Maheera Ayesha", 160, 29, 59000);
     bk2.terjual(5);
     bk2.tampilInformasi();
+
+    Buku16 bukuLuqman = new Buku16("Hujan Bulan Juni", "Sapardi Djoko Damono", 135, 5, 100000);
+        bukuLuqman.tampilInformasi();
+        bukuLuqman.terjual(2);
     }
 }

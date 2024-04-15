14997ef3cf6d036c046fc8a3ec8ef1cd91540e93
HaQi <luqmanananta745@gmail.com>
2024-04-16 04:32:14 +0700

    1

---
+++ Jobsheet 7/Barang16.java
@@ -0,0 +1,11 @@
+public class Barang16{
+    int kode;
+    String nama;
+    String kategori;
+
+    public Barang16(int kode, String nama, String kategori){
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+}
